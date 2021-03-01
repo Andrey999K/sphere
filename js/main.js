65c8880889d6245e3648f9126f4931b882d16a7f
Andrey999K <andrey1234xxxx@mail.ru>
2021-03-01 17:55:42 +0300

    add other locations

--- js/main.js
+++ js/main.js
@@ -1,4 +1,6 @@
 let scene, camera, renderer, sphereCamera;
+let number = 1;
+let number1 = 1;
 
 function init() {
     scene = new THREE.Scene();
@@ -14,9 +16,9 @@ function init() {
     controls.enableZoom = false;
 
     let urls = [
-        'img/posx.jpg', 'img/negx.jpg',
-        'img/posy.jpg', 'img/negy.jpg',
-        'img/posz.jpg', 'img/negz.jpg',
+        `img/${number}/posx.jpg`, `img/${number}/negx.jpg`,
+        `img/${number}/posy.jpg`, `img/${number}/negy.jpg`,
+        `img/${number}/posz.jpg`, `img/${number}/negz.jpg`,
     ];
     let loader = new THREE.CubeTextureLoader();
     scene.background = loader.load(urls);
@@ -46,5 +48,38 @@ function render() {
     renderer.render(scene, camera);
     sphereCamera.update(renderer, scene);
     requestAnimationFrame(render);
+    if (number > number1) {
+        number1 += 1;
+        const elem = document.querySelector("canvas");
+        document.body.removeChild(elem);
+        return;
+    } else if (number < number1) {
+        number1 -= 1;
+        const elem = document.querySelector("canvas");
+        document.body.removeChild(elem);
+        return;
+    }
 }
 init();
+
+const next = document.querySelector(".next");
+next.addEventListener("click", () => {
+    number += 1;
+    console.log(number, number1);
+    if (number == 19) {
+        number = 1;
+        number1 = 0;
+    }
+    init();
+});
+
+const back = document.querySelector(".back");
+back.addEventListener("click", () => {
+    number -= 1;
+    console.log(number, number1);
+    if (number == 0) {
+        number = 18;
+        number1 = 19;
+    }
+    init();
+});
